09ab789c048b8dae9e43e741a8bc9edfe7cdabcf
Lusques <lucas.carlos@toodoo.com.br>
2022-11-14 08:30:11 -0300

    pull

--- django/backend/microsoft_hackaton/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import MicrosoftHackaton
-
-class MicrosoftHackatonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MicrosoftHackaton
-        fields = ('id', 'title', 'description', 'completed')
