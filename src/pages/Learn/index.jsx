a2187169cd2516e9acaf4a95ba0c31daffa691ea
Lusques <lucas.carlos@toodoo.com.br>
2022-11-14 08:35:03 -0300

    .

--- src/pages/Learn/index.jsx
+++ src/pages/Learn/index.jsx
@@ -1,23 +1,18 @@
-<<<<<<< HEAD
+
 import "./style.scss"
 import Button from "../../components/Button";
-=======
->>>>>>> 948ee411960a6c8bf564b8ec57d2e2816fbca5dc
 import UserFooter from "../../components/UserFooter";
 import UserHeader from "../../components/UserHeader";
 const Learn = () => {
   return (
     <>
       <UserHeader />
-<<<<<<< HEAD
       <section className="learn-container d-flex justify-content-center align-item-center">
         <div className="">
           <Button text="Começar" />
         </div>
       </section>
-=======
       <p>Página Learn</p>
->>>>>>> 948ee411960a6c8bf564b8ec57d2e2816fbca5dc
       <UserFooter />
     </>
   );
