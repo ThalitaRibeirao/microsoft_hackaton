3d265e0b961319ad4701adafc91edc1b2558fddb
Lusques <lucas.carlos@toodoo.com.br>
2022-11-14 05:39:02 -0300

    page - certificates

--- microsoft_hackaton_app/src/Routes.jsx
+++ microsoft_hackaton_app/src/Routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Certificates from "./pages/Certificates";
 import Conquest from "./pages/Conquest";
 import Home from "./pages/home";
 import Learn from "./pages/Learn";
@@ -11,11 +12,12 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/learn" element={<Learn />}></Route>
+        <Route path="/certificates" element={<Certificates />}></Route>
         <Route path="/conquests" element={<Conquest />}></Route>
-        <Route path="/review" element={<Review />}></Route>
+        <Route path="/learn" element={<Learn />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/notifications" element={<Notifications />}></Route>
+        <Route path="/review" element={<Review />}></Route>
       </Routes>
     </Router>
   );
---
+++ microsoft_hackaton_app/src/pages/Certificates/index.jsx
@@ -0,0 +1,47 @@
+import "./style.scss";
+import UserFooter from "../../components/UserFooter";
+import "./style.scss";
+const Certificates = () => {
+  return (
+    <>
+      <section className="page-conquests page-notifications page-certificate bg-color-blue">
+        <h2 className="fw-bold mb-2 text-white text-center">Certificados</h2>
+        <h5 className="text-center text-white mb-4">
+          Suas certificações estão aqui
+        </h5>
+        <ul className="notifications-list certificate-list list-unstyled">
+          <li className="notifications-list-item certificate-list-item my-5">
+            <figure className="notifications-item-image certificate-list mb-3">
+              <img className="w-100" src="./img/insignia-humanas.png" alt="" />
+              <figure className="certificate-image-float">
+                <img
+                  className="w-100"
+                  src="./img/export-certificate.svg"
+                  alt=""
+                />
+              </figure>
+            </figure>
+            <h3 className="text-center text-white">Língua Portuguesa</h3>
+            <p className="text-white text-center">Unidades 1 a 30</p>
+          </li>
+          <li className="notifications-list-item certificate-list-item my-5">
+            <figure className="notifications-item-image certificate-list mb-3">
+              <img className="w-100" src="./img/insignia-humanas.png" alt="" />
+              <figure className="certificate-image-float">
+                <img
+                  className="w-100"
+                  src="./img/export-certificate.svg"
+                  alt=""
+                />
+              </figure>
+            </figure>
+            <h3 className="text-center text-white">Língua Portuguesa</h3>
+            <p className="text-white text-center">Unidades 1 a 30</p>
+          </li>
+        </ul>
+      </section>
+      <UserFooter page="conquest" />
+    </>
+  );
+};
+export default Certificates;
